c2681a9a7ac8f531e2d195ce5c687bb28a2e487e
Hendry Putra Kurniawan <hendryputra934@gmail.com>
2026-06-19 16:43:56 +0700

    add logo

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -3,14 +3,9 @@ import AppLogoIcon from '@/components/AppLogoIcon.vue';
 </script>
 
 <template>
-    <div
-        class="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground"
-    >
-        <AppLogoIcon class="size-5 fill-current text-white dark:text-black" />
-    </div>
-    <div class="ml-1 grid flex-1 text-left text-sm">
-        <span class="mb-0.5 truncate leading-tight font-semibold"
-            >Laravel Starter Kit</span
-        >
+    <div class="flex size-30 items-center justify-center">
+        <AppLogoIcon 
+            class="size-full max-h-[90%] object-contain fill-current text-sidebar-primary dark:text-white" 
+        />
     </div>
 </template>
